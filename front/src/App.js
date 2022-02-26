fe6893125d28dee02c04678a748e186edf99f00d
Pavlo Mishchenko <pavlo@avatour.live>
2022-02-26 18:25:53 +0100

    finish form

--- front/src/App.js
+++ front/src/App.js
@@ -2,23 +2,39 @@ import './App.css';
 import {useRef, useState, useCallback} from "react";
 import {useDropzone} from 'react-dropzone'
 import noImg from "./img.png";
+import axios from "axios";
 
 function App() {
   const [text, setText] = useState('');
   const img = useRef(null);
+  const fileRef = useRef(null);
+
   const onDrop = useCallback(acceptedFiles => {
     const reader = new FileReader()
-    console.log('acceptedFiles', acceptedFiles)
-    const file = acceptedFiles[0];
+    const _file = acceptedFiles[0];
     reader.onload = () => {
       // Do whatever you want with the file contents
       const binaryStr = reader.result
-      console.log(binaryStr)
       img.current.src = binaryStr;
     }
-    reader.readAsDataURL(file)
+    reader.readAsDataURL(_file)
+    fileRef.current = _file;
   }, [])
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  const submitData = () => {
+    const file = fileRef.current;
+    console.log('file', file)
+    console.log('text', text)
+    if (!fileRef || !text.length) return;
+
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('text', text);
+
+    // axios.post('/upload/image', {body: formData});
+    console.log(formData)
+  }
 
   const changeText = ({target: {value}}) => setText(value)
   return (
@@ -27,9 +43,10 @@ function App() {
         <p>
           Отправить привет с украины без смс и регистрации.
         </p>
+        <button onClick={submitData}>Отправить</button>
         <textarea value={text} onChange={changeText}/>
         <div {...getRootProps()}>
-          <input {...getInputProps()} />
+          <input {...getInputProps()}/>
           {
             isDragActive ?
                 <p>Drop the files here ...</p> :
