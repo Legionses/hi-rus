e81a696631b8bbdc7b4881eb1b2c2023068e5b6f
Oleksandr Palokha <palokha.oa@gmail.com>
2022-02-27 20:18:21 -0500

    Added DKIM

--- back/mailer.js
+++ back/mailer.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
-const {MAIL_USER, MAIL_PASS } = process.env;
+const {MAIL_USER, MAIL_PASS, DKIM } = process.env;
 async function getTransporter() {
   let transporter = nodemailer.createTransport({
     host: "smtp.hi-russian.com",
     port: 7005,
     ignoreTLS: true,
+    dkim: {
+      domainName: "smtp.hi-russian.com",
+      keySelector: "s1",
+      privateKey: DKIM,
+    },
     auth: {
       user: MAIL_USER,
       pass: MAIL_PASS
